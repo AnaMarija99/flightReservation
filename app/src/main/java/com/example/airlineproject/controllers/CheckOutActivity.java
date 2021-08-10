5037d426b05c0828dfd9cbb6e287cd3f4a6730ed
unknown <ana.jovanoska4@gmail.com>
2021-08-10 15:38:21 +0200

    First commit

---
+++ app/src/main/java/com/example/airlineproject/controllers/CheckOutActivity.java
@@ -0,0 +1,57 @@
+package com.example.airlineproject.controllers;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import com.example.airlineproject.R;
+
+public class CheckOutActivity extends AppCompatActivity {
+
+    private SharedPreferences sharedPreferences;
+    private int currentTab;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_out);
+
+        sharedPreferences = getSharedPreferences("PREFS", Context.MODE_PRIVATE);
+        currentTab = sharedPreferences.getInt("CURRENT_TAB", 0);
+
+        //Toast.makeText(getApplicationContext(), String.valueOf(currentTab), Toast.LENGTH_SHORT).show();
+
+
+        if (currentTab == 0) {
+            // Create new fragment and transaction
+            CheckoutOnewayFragment newFragment = new CheckoutOnewayFragment();
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+            // Replace whatever is in the fragment_container view with this fragment,
+            // and add the transaction to the back stack
+            transaction.replace(R.id.fragment_container, newFragment);
+            transaction.addToBackStack(null);
+
+            // Commit the transaction
+            transaction.commit();
+        } else if (currentTab == 1) {
+            // Create new fragment and transaction
+            CheckoutRoundFragment newFragment = new CheckoutRoundFragment();
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+            // Replace whatever is in the fragment_container view with this fragment,
+            // and add the transaction to the back stack
+            transaction.replace(R.id.fragment_container, newFragment);
+            transaction.addToBackStack(null);
+
+            // Commit the transaction
+            transaction.commit();
+        }
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/airlineproject/models/Itinerary.java
@@ -0,0 +1,25 @@
+package com.example.airlineproject.models;
+
+import java.util.Date;
+
+public class Itinerary {
+
+    private int itineraryID;
+    private Date timeStamp;
+
+    public int getItineraryID() {
+        return itineraryID;
+    }
+
+    public void setItineraryID(int itineraryID) {
+        this.itineraryID = itineraryID;
+    }
+
+    public Date getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Date timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+}
